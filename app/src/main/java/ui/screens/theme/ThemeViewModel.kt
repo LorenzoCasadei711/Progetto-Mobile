58c308a757df623961cf5c686f724231a5ee1952
Lorenzo Casadei <lorenzo.casadei16@studio.unibo.it>
2026-04-26 16:25:29 +0200

    example of home page

---
+++ app/src/main/java/ui/screens/theme/ThemeViewModel.kt
@@ -0,0 +1,9 @@
+package ui.screens.theme
+
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import ui.screens.SettingsScreen
+
